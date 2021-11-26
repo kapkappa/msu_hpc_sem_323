3102609564f129a0b045c671ac473eb3a1934a6c
roman <roman.kupry@gmail.com>
2021-11-27 02:22:44 +0300

    small fixes

--- problem3/main.c
+++ problem3/main.c
@@ -1,136 +1,123 @@
-#include <string.h>
-#include <unistd.h>
+#include <assert.h>
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <omp.h>
-#include <assert.h>
+#include <string.h>
+#include <unistd.h>
 
 #define MAX_THREADS_NUMBER omp_get_max_threads()
 int nthreads = 1;
 
-double time_start, time_end;
-
-void fill_array(int* array, int array_size) {
-    int i;
-    for (i = 0; i < array_size; i++) {
-        array[i] = rand();
-    }
+void fill_array(int *array, int array_size) {
+  int i;
+  for (i = 0; i < array_size; i++) {
+    array[i] = rand();
+  }
 }
 
-int compare_ints(const void* a, const void* b) {
-    int arg1 = *(const int*)a;
-    int arg2 = *(const int*)b;
-    if (arg1 < arg2) return -1;
-    if (arg1 > arg2) return 1;
-    return 0;
+int compare_ints(const void *a, const void *b) {
+  int arg1 = *(const int *)a;
+  int arg2 = *(const int *)b;
+  if (arg1 < arg2)
+    return -1;
+  if (arg1 > arg2)
+    return 1;
+  return 0;
 }
 
+void merge(int *array, int middle, int array_size, int *tmp) {
+  int i = 0, j = middle, ti = 0;
 
-void merge(int* array, int middle, int array_size, int* tmp) {
-
-    int i = 0, j = middle, ti = 0;
-
-   while (i < middle && j < array_size) {
-      if (array[i] < array[j]) {
-         tmp[ti] = array[i];
-         ti++;
-         i++;
-      } else {
-         tmp[ti] = array[j];
-         ti++;
-         j++;
-      }
-   }
-   while (i<middle) {
-      tmp[ti] = array[i];
-      ti++;
-      i++;
-   }
-   while (j<array_size) {
-      tmp[ti] = array[j];
-      ti++;
-      j++;
-   }
-   memcpy(array, tmp, array_size*sizeof(int));
+  while (i < middle && j < array_size) {
+    if (array[i] < array[j]) {
+      tmp[ti++] = array[i++];
+    } else {
+      tmp[ti++] = array[j++];
+    }
+  }
+  for (i; i < middle; i++) {
+    tmp[ti++] = array[i];
+  }
+  for (j; j < array_size; j++) {
+    tmp[ti++] = array[j];
+  }
+  memcpy(array, tmp, array_size * sizeof(int));
 }
 
-void merge_sort(int* array, int array_size, int* tmp) {
-    if (array_size < 2) return;
-    int i = 0;
-    int chunk_size = array_size / nthreads;
-    int extra_chunk = array_size % nthreads;
-/*
-    while (i < nthreads) {
-        if (i == 0) extra_chunk = array_size % nthreads;
-    #pragma omp task shared(array)
-    {
-        qsort(array+i*chunk_size+extra_chunk, chunk_size+extra_chunk, sizeof(int), compare_ints);
-    }
-        i++;
-    }
-*/
-    for (i = 0; i < nthreads; i++) {
-        if (i == 0) {
-            chunk_size += extra_chunk;
-            extra_chunk = 0;
-        }
-    #pragma omp task shared(array)
-        qsort(array+ i*chunk_size + extra_chunk, chunk_size, sizeof(int), compare_ints);
-    }
+void merge_sort(int *array, int array_size, int *tmp) {
+  if (array_size < 2)
+    return;
+  int i = 0;
+  int chunk_size = array_size / nthreads;
+  int extra_chunk = array_size % nthreads;
 
+  for (i = 0; i < nthreads; i++) {
+    if (i == 0) {
+      chunk_size += extra_chunk;
+      extra_chunk = 0;
+    }
+#pragma omp task shared(array)
+    qsort(array + i * chunk_size + extra_chunk, chunk_size, sizeof(int),
+          compare_ints);
+  }
 
 #pragma omp taskwait
-    for (i = 1; i < nthreads; i++) {
-        int current_border = chunk_size * i + extra_chunk;
-        int current_size = current_border + chunk_size;
-        merge(array, current_border, current_size, tmp);
-    }
+  for (i = 1; i < nthreads; i++) {
+    int current_border = chunk_size * i + extra_chunk;
+    int current_size = current_border + chunk_size;
+    merge(array, current_border, current_size, tmp);
+  }
 }
 
-int is_sorted(int* array, int array_size) {
-    int i;
-    for (i = 1; i < array_size; i++)
-        if (array[i] < array[i-1]) return 0;
+int is_sorted(int *array, int array_size) {
+  int i;
+  for (i = 1; i < array_size; i++)
+    if (array[i] < array[i - 1])
+      return 0;
+  return 1;
+}
+
+int main(int argc, char **argv) {
+  srand(123);
+
+  int array_size;
+
+  array_size = (argc > 1) ? atoi(argv[1]) : 1000000;
+  assert(array_size > 0);
+  nthreads = (argc > 2) ? atoi(argv[2]) : 1;
+  assert(nthreads > 0 && nthreads <= MAX_THREADS_NUMBER);
+
+  omp_set_dynamic(0);
+  omp_set_num_threads(nthreads);
+  printf("nthreads: %d\n", nthreads);
+
+  int *array = (int *)malloc(sizeof(int) * array_size);
+  int *tmp = (int *)malloc(sizeof(int) * array_size);
+
+  if (!array || !tmp) {
+    if (array)
+      free(array);
+    if (tmp)
+      free(tmp);
     return 1;
-}
+  }
 
-int main(int argc, char**argv) {
-    srand(123);
+  fill_array(array, array_size);
 
-    int array_size;
+  double time_start, time_end;
 
-    array_size  = (argc > 1) ? atoi(argv[1]) : 1000000;
-    assert(array_size > 0);
-    nthreads = (argc > 2) ? atoi(argv[2]) : 1;
-    assert(nthreads > 0 && nthreads <= MAX_THREADS_NUMBER);
-
-    omp_set_dynamic(0);
-    omp_set_num_threads(nthreads);
-    printf("nthreads: %d\n", nthreads);
-
-    int* array = (int*)malloc(sizeof(int) * array_size);
-    int* tmp = (int*)malloc(sizeof(int) * array_size);
-
-    if (!array || !tmp) {
-        if(array) free(array);
-        if(tmp) free(tmp);
-        return 1;
-    }
-
-    fill_array(array, array_size);
-
-    time_start = omp_get_wtime();
+  time_start = omp_get_wtime();
 #pragma omp parallel
-{
-    #pragma omp single
+  {
+#pragma omp single
     merge_sort(array, array_size, tmp);
-}
-    time_end = omp_get_wtime();
+  }
+  time_end = omp_get_wtime();
 
-    assert(is_sorted(array, array_size) == 1);
+  assert(is_sorted(array, array_size) == 1);
 
-    printf("sort time: %f\n", time_end - time_start);
-    free(array);
-    free(tmp);
-    return 0;
+  printf("parallel sort time: %f\n", time_end - time_start);
+  free(array);
+  free(tmp);
+  return 0;
 }
--- problem3/qsort.c
+++ problem3/qsort.c
@@ -1,68 +1,70 @@
-#include <sys/time.h>
-#include <string.h>
-#include <unistd.h>
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
-
-double time_start, time_end;
-int nthreads = 1;
+#include <string.h>
+#include <sys/time.h>
+#include <unistd.h>
 
 double timer() {
-    struct timeval tp;
-    struct timezone tzp;
-    gettimeofday(&tp, &tzp);
-    return ((double)tp.tv_sec + (double)tp.tv_usec * 1.e-6);
+  struct timeval tp;
+  struct timezone tzp;
+  gettimeofday(&tp, &tzp);
+  return ((double)tp.tv_sec + (double)tp.tv_usec * 1.e-6);
 }
 
-void fill_array(int* array, int array_size) {
-    int i;
-    for (i = 0; i < array_size; i++) {
-        array[i] = rand();
-    }
+void fill_array(int *array, int array_size) {
+  int i;
+  for (i = 0; i < array_size; i++) {
+    array[i] = rand();
+  }
 }
 
-int compare_ints(const void* a, const void* b)
-{
-    int arg1 = *(const int*)a;
-    int arg2 = *(const int*)b;
+int compare_ints(const void *a, const void *b) {
+  int arg1 = *(const int *)a;
+  int arg2 = *(const int *)b;
 
-    if (arg1 < arg2) return -1;
-    if (arg1 > arg2) return 1;
-    return 0;
-
-    // return (arg1 > arg2) - (arg1 < arg2); // possible shortcut
-    // return arg1 - arg2; // erroneous shortcut (fails if INT_MIN is present)
-}
-
-int is_sorted(int* array, int array_size) {
-    int i;
-    for (i = 1; i < array_size; i++)
-        if (array[i-1] > array[i]) return 0;
+  if (arg1 < arg2)
+    return -1;
+  if (arg1 > arg2)
     return 1;
+  return 0;
+
+  // return (arg1 > arg2) - (arg1 < arg2); // possible shortcut
+  // return arg1 - arg2; // erroneous shortcut (fails if INT_MIN is present)
 }
 
-int main(int argc, char**argv) {
-    srand(123);
+int is_sorted(int *array, int array_size) {
+  int i;
+  for (i = 1; i < array_size; i++)
+    if (array[i - 1] > array[i])
+      return 0;
+  return 1;
+}
 
-    int array_size;
-    array_size  = (argc > 1) ? atoi(argv[1]) : 10000;
-    assert(array_size > 0);
+int main(int argc, char **argv) {
+  srand(123);
 
-    int* array = (int*)malloc(sizeof(int) * array_size);
-    if(!array) return 1;
+  int array_size;
+  array_size = (argc > 1) ? atoi(argv[1]) : 1000000;
+  assert(array_size > 0);
 
-    fill_array(array, array_size);
+  int *array = (int *)malloc(sizeof(int) * array_size);
+  if (!array)
+    return 1;
 
-    time_start = timer();
+  fill_array(array, array_size);
 
-       qsort(array, array_size, sizeof(int), compare_ints);
+  double time_start, time_end;
 
-    time_end = timer();
+  time_start = timer();
 
-    assert(is_sorted(array, array_size) == 1);
+  qsort(array, array_size, sizeof(int), compare_ints);
 
-    printf("sort time: %f\n", time_end - time_start);
-    free(array);
-    return 0;
+  time_end = timer();
+
+  assert(is_sorted(array, array_size) == 1);
+
+  printf("qsort time: %f\n", time_end - time_start);
+  free(array);
+  return 0;
 }
