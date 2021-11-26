8f95f5f8d03dd5fa473b7651a698157fe472d82d
roman <roman.kupry@gmail.com>
2021-11-26 21:39:22 +0300

    first implementation

--- problem3/main.c
+++ problem3/main.c
@@ -1,14 +1,16 @@
+#include <string.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <assert.h>
 
 #define MAX_THREADS_NUMBER omp_get_max_threads()
 
 double time_start, time_end;
 int nthreads = 1;
 
-void fill_array(int64_t* array, int array_size) {
+void fill_array(int* array, int array_size) {
     int i;
     srand(123);
     for (i = 0; i < array_size; i++) {
@@ -16,23 +18,51 @@ void fill_array(int64_t* array, int array_size) {
     }
 }
 
-parallel_sort(int64_t*array, int array_size) {
+void merge(int* array, int array_size, int* tmp) {
+    int i = 0, j = array_size / 2, ti = 0;
+
+   while (i < array_size / 2 && j < array_size) {
+      if (array[i] < array[j]) {
+         tmp[ti] = array[i];
+         ti++;
+         i++;
+      } else {
+         tmp[ti] = array[j];
+         ti++;
+         j++;
+      }
+   }
+   while (i<array_size/2) { /* finish up lower half */
+      tmp[ti] = array[i];
+      ti++;
+      i++;
+   }
+   while (j<array_size) { /* finish up upper half */
+      tmp[ti] = array[j];
+      ti++;
+      j++;
+   }
+   memcpy(array, tmp, array_size*sizeof(int));
+}
+
+void merge_sort(int* array, int array_size, int* tmp) {
+    if (array_size < 2) return;
+
+#pragma omp task shared(array) if(array_size > 100)
+    merge_sort(array, array_size/2, tmp);
+
+#pragma omp task shared(array) if(array_size > 100)
+    merge_sort(array+(array_size/2), array_size - (array_size/2), tmp + (array_size/2));
+
+#pragma omp taskwait
+    merge(array, array_size, tmp);
+}
+
+int is_sorted(int* array, int array_size) {
     int i;
-
-     omp_set_num_threads(nthreads);
-     printf("nthreads: %d\n", nthreads);
-
-//1. divide array to (array_size / nthreads) chunks
-
-
-//2. every threads work with its own chunk
-#pragma omp parallel for private(i) shared(array) schedule(static)
-        for (i = 0; i < array_size; i++)
-            printf("%d ", array[i]);
-    printf("\n");
-
-//3. sort chunks
-
+    for (i = 1; i < array_size; i++)
+        if (array[i] < array[i-1]) return 0;
+    return 1;
 }
 
 int main(int argc, char**argv) {
@@ -49,15 +79,33 @@ int main(int argc, char**argv) {
         scanf("%d", &nthreads);
     } while ((nthreads <=0) || (nthreads > MAX_THREADS_NUMBER ));
 
+    omp_set_dynamic(0);
+    omp_set_num_threads(nthreads);
+    printf("nthreads: %d\n", nthreads);
 
-    int64_t* array = (int64_t*)malloc(sizeof(int64_t) * array_size);
+    int* array = (int*)malloc(sizeof(int) * array_size);
+    int* tmp = (int*)malloc(sizeof(int) * array_size);
+
+    if (!array || !tmp) {
+        if(array) free(array);
+        if(tmp) free(tmp);
+        return 1;
+    }
+
     fill_array(array, array_size);
 
     time_start = omp_get_wtime();
-    parallel_sort(array, array_size);
+#pragma omp parallel
+{
+    #pragma omp single
+       merge_sort(array, array_size, tmp);
+}
     time_end = omp_get_wtime();
+
+    assert(is_sorted(array, array_size) == 1);
 
     printf("sort time: %f\n", time_end - time_start);
     free(array);
+    free(tmp);
     return 0;
 }
