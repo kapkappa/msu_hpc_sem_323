13bd0bd7a7754bcba2486c8aff24af80cc836916
roman <roman.kupry@gmail.com>
2021-11-26 16:04:39 +0300

    summary started

--- problem0/test.sh
+++ problem0/test.sh
@@ -1,16 +1,55 @@
 #!/bin/bash
 
-echo "Check with adress sanitizer"
-./test 10000000000 6
+echo "10^8 segments, with 1 threads"
+./prog 100000000 1
 
-echo "10 billion segments, with 1 threads"
+echo "10^8 segments, with 2 threads"
+./prog 100000000 2
+
+echo "10^8 segments, with 4 threads"
+./prog 100000000 4
+
+echo "10^8 segments, with 6 threads"
+./prog 100000000 6
+
+echo "10^8 segments, with 8 threads"
+./prog 100000000 8
+
+echo "10^8 segments, with 12 threads"
+./prog 100000000 12
+
+echo "10^9 segments, with 1 threads"
+./prog 1000000000 1
+
+echo "10^9 segments, with 2 threads"
+./prog 1000000000 2
+
+echo "10^9 segments, with 4 threads"
+./prog 1000000000 4
+
+echo "10^9 segments, with 6 threads"
+./prog 1000000000 6
+
+echo "10^9 segments, with 8 threads"
+./prog 1000000000 8
+
+echo "10^9 segments, with 12 threads"
+./prog 1000000000 12
+
+echo "10^10 segments, with 1 threads"
 ./prog 10000000000 1
 
-echo "10 billion segments, with 2 threads"
+echo "10^10 segments, with 2 threads"
 ./prog 10000000000 2
 
-echo "10 billion segments, with 4 threads"
+echo "10^10 segments, with 4 threads"
 ./prog 10000000000 4
 
-echo "10 billion segments, with 10 threads"
-./prog 10000000000 10
+echo "10^10 segments, with 6 threads"
+./prog 10000000000 6
+
+echo "10^10 segments, with 8 threads"
+./prog 10000000000 8
+
+echo "10^10 segments, with 12 threads"
+./prog 10000000000 12
